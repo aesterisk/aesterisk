57080fa18fed4f8d38a47778692f563760d8228c
yolocat-dev <developer@yolocat.dev>
2024-12-26 13:23:28 +0100

    🚨(lint): fix linting errors

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import React from "react";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -21,7 +22,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
 	return (
 		<html lang="en">
 			<body className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}>
-				{children}
+				{ children }
 			</body>
 		</html>
 	);
