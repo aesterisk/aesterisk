0450b2fcb6282a77d359a18fce1a212ea5e5d0aa
yolocat-dev <developer@yolocat.dev>
2024-12-30 13:55:49 +0100

    💻(teams): added debug component for `/dash/[team]` and `/dash/[team]/servers`

--- web/app/dash/[team]/servers/page.tsx
+++ web/app/dash/[team]/servers/page.tsx
@@ -1,11 +1,11 @@
 import TeamTest from "@/components/app/team-test";
 
-export default function Home({ params }: { params: Promise<{ team: string; }>; }) {
+export default function Servers({ params }: { params: Promise<{ team: string; }>; }) {
 	const team = params.then((p) => p.team);
 
 	return (
 		<main className="px-4 py-5 w-full">
-			<span>{ "Hello, world!" }</span>
+			<span>{ "Servers" }</span>
 			<TeamTest teamID={team} />
 		</main>
 	);
