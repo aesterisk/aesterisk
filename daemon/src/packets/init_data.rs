0673a931768f69b4fd95f098f21a999b1bffb55b
yolocat-dev <developer@yolocat.dev>
2025-03-22 17:52:38 +0100

    ✨(networks): daemon creates assigned networks at startup

--- daemon/src/packets/init_data.rs
+++ daemon/src/packets/init_data.rs
@@ -1,8 +1,19 @@
 use packet::server_daemon::init_data::SDInitDataPacket;
-use tracing::debug;
+use tracing::{debug, info};
+
+use crate::docker;
 
 pub async fn handle(init_data_packet: SDInitDataPacket) -> Result<(), String> {
-    debug!("Received InitDataPacket: {:#?}", init_data_packet);
+    info!("Syncing data from server with Docker");
+
+    for nw in init_data_packet.networks {
+        debug!("Checking network {}", nw.id);
+        if !docker::network::network_exists(nw.id).await? {
+            debug!("Creating network {}", nw.id);
+            let id = docker::network::create_network(nw.id, nw.subnet).await?;
+            debug!("Created network ({})", id);
+        }
+    }
 
     Ok(())
 }
