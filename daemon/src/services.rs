2f5a2a39f7e4f421a0bab50f04b4c28905df515b
yolocat-dev <developer@yolocat.dev>
2025-04-17 10:21:02 +0200

    🔥(servers): remove unused server status service

--- daemon/src/services.rs
+++ daemon/src/services.rs
@@ -3,13 +3,11 @@ use tokio_util::sync::CancellationToken;
 
 mod client;
 mod node_status;
-mod server_status;
 
 /// Starts the services and returns their join handles
 pub fn start(token: CancellationToken) -> Vec<JoinHandle<Result<(), String>>> {
     vec![
         tokio::spawn(client::run(token.clone())),
-        tokio::spawn(node_status::run(token.clone())),
-        tokio::spawn(server_status::run(token)),
+        tokio::spawn(node_status::run(token)),
     ]
 }
